5f51b8ff9a9668f5c9a9e37f6fb3adadfb63799f
ameer017 <rajiabdullahi907@gmail.com>
2026-04-03 23:11:22 +0100

    unload the repo

--- talent-server/routes/contactRoute.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const { createForm } = require("../controllers/contactController");
-const router = express.Router();
-
-router.post("/create-form", createForm);
-module.exports = router;
